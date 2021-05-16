7ad34c3db88257fbba06fa181c8242b6e020560d
Jackie Leary <jackie.leary02@gmail.com>
2021-05-16 15:40:45 -0400

    Add AsteroidCard component

--- space-project/src/App.js
+++ space-project/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Navbar from './components/Navbar';
 import DateSearch from './components/DateSearch';
 import AsteroidDetails from './components/AsteroidDetails';
+import AsteroidCard from './components/AsteroidCard';
+
 
 function App() {
   return (
@@ -11,6 +13,11 @@ function App() {
           <Navbar />
           <DateSearch />
           <AsteroidDetails />
+
+          <div className="row">
+            <div className="col-md-auto"><AsteroidCard name="Asteroid 1"/></div>
+            <div className="col-md-auto"><AsteroidCard name="Asteroid 2"/></div>
+          </div>
           
         </div>
       </div>
