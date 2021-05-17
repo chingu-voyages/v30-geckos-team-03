bff701214ca400272a01fde26d3bb26f717b6b5c
Tiffany Udoh <tiffany.udoh@gmail.com>
2021-05-16 23:54:18 -0400

    Add env variables and return API sample data

--- space-project/src/App.js
+++ space-project/src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
+import React, { Component } from "react";
+import FetchSpaceInfo from "./FetchSpaceInfo";
 
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    visible: true
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <FetchSpaceInfo />
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// function App() {
+//   let information='chocolate mint';
+  
+//   return (
+//     <div className="App">
+//     <p>Function Variable Data Below</p>
+//     <p>{information}</p>
+//     </div>
+//   );
+// }
+
+// export default App;
