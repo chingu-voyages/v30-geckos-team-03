2b37361fc342539544f46017fd094ecec5adb8c1
Tiffany Udoh <tiffany.udoh@gmail.com>
2021-05-23 15:56:39 -0400

    Return same data from a list of asteroids, part 1

--- space-project/src/components/DateSearch.jsx
+++ space-project/src/components/DateSearch.jsx
@@ -6,7 +6,7 @@ function DateSearch(){
         <div>
             <p>Search for Asteroids based on their closest approach date to Earth: </p>
           <div className="row">
-            <div className="col-md-2"><Searchbar placeholder="start MM/DD/YYYY"/></div>
+            <div className="col-md-2"><Searchbar placeholder="start MM/DD/YYYY" /></div>
             <div className="col-md-2"><Searchbar placeholder="end MM/DD/YYYY" /></div>
             <div className="col-md-auto"><button type="button" class="btn btn-light search-button"><i class="fas fa-search"></i></button></div>
           </div>
