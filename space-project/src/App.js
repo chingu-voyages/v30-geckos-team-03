76071b6dcbebb1bc52ee5f4b90b844d241e70eb6
Jackie Leary <jackie.leary02@gmail.com>
2021-06-03 20:11:23 -0400

    Add sign up page and react-router-dom

--- space-project/src/App.js
+++ space-project/src/App.js
@@ -8,28 +8,41 @@ import AsteroidCard from './components/AsteroidCard';
 import Image from './components/Image';
 import MoreInfo from './components/MoreInfo';
 import FetchSpaceInfo from './components/FetchSpaceInfo';
+import Signup from './components/Signup';
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 
 export const App = () => {
   
   return (
-    <div className="App">
-      <div className="container-fluid">
-        <div className="container-border">
-          <Navbar />
-          <DateSearch />
-          {/* <AsteroidDetails/> */}
-          <FetchSpaceInfo />
+    <Router>
+      <div className="App">
+        <div className="container-fluid">
+          <div className="container-border">
+          <Switch>
+            <Route exact path="/">
+              <Navbar />
+              <DateSearch />
+              {/* <AsteroidDetails/> */}
+              {/* <FetchSpaceInfo /> */}
 
-          {/* <div className="row">
-            <div className="col-md-auto"><AsteroidCard name="Asteroid 1"/></div>
-            <div className="col-md-auto"><AsteroidCard name="Asteroid 2"/></div>
-          </div> */}
-          
-          <Image />
+              {/* <div className="row">
+                <div className="col-md-auto"><AsteroidCard name="Asteroid 1"/></div>
+                <div className="col-md-auto"><AsteroidCard name="Asteroid 2"/></div>
+              </div> */}
+              
+              <Image />
+            </Route>
+
+            <Route exact path="/Signup">
+              <Signup />
+            </Route>
+
+          </Switch>
+          </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 };
 
