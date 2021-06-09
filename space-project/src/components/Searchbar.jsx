39fdfa4212aaf9424922af250c01073310d0ae59
Tiffany Udoh <tiffany.udoh@gmail.com>
2021-06-09 14:10:01 -0400

    Add back Searchbar code

--- space-project/src/components/Searchbar.jsx
+++ space-project/src/components/Searchbar.jsx
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import AsteroidDetails from "./AsteroidDetails";
+import fetchAsteroid from "../FetchSpaceInfo.js";
 
-function Searchbar(props){
-    return(
-        <div>
-            <input className="searchbar form-control" type="text" placeholder={props.placeholder}/>
+const Searchbar = (props) => {
+  const [startDate, setStartDate] = useState("");
+  const endDate = startDate;
+  const [asteroid, setAsteroid] = useState({});
+  
+
+  const searchAsteroid = async (e) => {
+    if (e.key === "Enter") {
+      const dataAsteroid = fetchAsteroid(startDate, endDate);
+
+      setAsteroid(dataAsteroid);
+      setStartDate("");
+    } else {
+      const dataAsteroid = fetchAsteroid(startDate, endDate);
+
+      setAsteroid(dataAsteroid);
+      setStartDate("");
+    }
+  };
+
+  
+
+  return (
+    <>
+      <div className="row">
+        <div className="col-md-4">
+          <input
+            className="searchbar form-control"
+            type="date"
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+            onKeyPress={searchAsteroid}
+            placeholder="Search..."
+          />
         </div>
-        
-    );
-}
+        <div className="col-md-2">
+          <button
+            type="button"
+            className="btn btn-light search-button"
+            onClick={searchAsteroid}
+          >
+            <i className="fas fa-search"></i>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Searchbar;
