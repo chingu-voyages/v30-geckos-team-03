88df3b922257715146743591330fafcd6e656909
Tiffany Udoh <tiffany.udoh@gmail.com>
2021-06-12 01:16:15 -0400

    Heroku details take 2

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
+
+const port = process.env.PORT || 3001
 
 //connect to mongoose
 mongoose.connect("mongodb+srv://jackie-admin:asteroid123@cluster0.mma4u.mongodb.net/asteroidDB")
@@ -12,10 +15,18 @@ mongoose.connect("mongodb+srv://jackie-admin:asteroid123@cluster0.mma4u.mongodb.
 //require route
 app.use("/", require("./routes/userRoute"));
 
+if (process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname, '/space-project/build')))
+
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, 'space-project','build','index.html'));
+
+})}
+else{
 app.get("/", function(req, res) {
     res.send("Express here!");
-})
+})}
 
-app.listen(3001, function() {
-    console.log("express server is running on port 3001")
+app.listen(port, function() {
+    console.log("express server is running on port", port);
 })
